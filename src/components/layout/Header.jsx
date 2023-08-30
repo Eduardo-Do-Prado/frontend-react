4b6d74837128bd27dc622904e6e74e282a881f5e
Eduardo Luiz <dudsprado999@gmail.com>
2023-08-29 22:32:44 -0300

    29.08 completo

---
+++ src/components/layout/Header.jsx
@@ -0,0 +1,18 @@
+import './Header.css'
+
+export const Header = () => {
+  return (
+    <header id ='header'>
+        <h1>Logo</h1>
+        <nav>
+            <ul>
+                <li>home</li>
+                <li>Quem somos</li>
+                <li>Contato</li>
+            </ul>
+        </nav>
+    </header>
+  )
+}
+
+export default Header;
